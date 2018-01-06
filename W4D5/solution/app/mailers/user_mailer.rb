1a3b8aad6784ec5bcfa17bb34e9b91e651aed33a
Adrian Horning <m.adrian.horning@gmail.com>
2018-01-05 21:37:31 -0800

    Finish testing exercise on music app

--- W4D5/solution/app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default from: "everybody@appacademy.io"
-
-  def welcome_email(user)
-    @user = user
-    @url  = 'http://example.com/session/new'
-    mail(to: user.username, subject: 'Welcome to 99 Cats')
-  end
-end
---
+++ W5D1/music_app1/app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login_user(@user)
+      redirect_to user_url(@user.id)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+    end
+  end
+
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def show
+    render :show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
